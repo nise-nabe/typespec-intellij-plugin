a6d5f4ad84c87b4a31414a92b133c1b254d2a0f3
nise_nabe <nise.nabe@gmail.com>
2026-03-30 08:09:06 +0900

    update project name to `TypeSpecIntellijPlugin` in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
 
-rootProject.name = "TypeSpecPlugin"
+rootProject.name = "TypeSpecIntellijPlugin"
 
 pluginManagement {
     repositories {
